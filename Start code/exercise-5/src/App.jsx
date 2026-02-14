bd09c20a43e0b6041fcfdf8a3c879d9853a04348
Keazz1342 <namkea669@gmail.com>
2026-02-15 00:06:50 +0700

    week 5

--- Start code/exercise-5/src/App.jsx
+++ Start code/exercise-5/src/App.jsx
@@ -1,5 +1,38 @@
+
+import { PEOPLE_DATA } from "./data.js";
+
 function App() {
-  return <>{/* Your code  here */}</>;
+  return (
+    <>
+      <header>
+        <h1>My Experience</h1>
+      </header>
+      <div className="cards-view">
+        <div className="cards-grid">
+          {PEOPLE_DATA.map((person) => (
+            <Card
+              key={person.id}
+              name={person.name}
+              subtitle={person.subtitle}
+              description={person.description}
+              image={person.image}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Card({ name, subtitle, description, image }) {
+  return (
+    <div className="card">
+      <img src={image} alt={name} className="card-image" />
+      <h2>{name}</h2>
+      <p className="subtitle">{subtitle}</p>
+      <p className="description">{description}</p>
+    </div>
+  );  
 }
 
 export default App;
--- Start code/exercise-5/src/data.js
+++ Start code/exercise-5/src/data.js
@@ -1,1 +1,30 @@
-/* Your data here */
+import img1 from "./assets/Student.photo.jpg";
+import img2 from "./assets/cute.photo.jpg";
+import img3 from "./assets/leader.photo.jpg";
+
+export const PEOPLE_DATA = [
+  {
+    id: 1,
+    name: "Namkea",
+    subtitle: "Software Developer",
+    description:
+      "Loves coding and building web applications. Enjoys learning new technologies and solving complex problems.",
+    image: img1,
+  },
+  {
+    id: 2,
+    name: "Namkea",
+    subtitle: "Graphic Designer",
+    description:
+      "Passionate about creating beautiful designs. Specializes in UI/UX and brand identity design.",
+    image: img2,
+  },
+  {
+    id: 3,
+    name: "Namkea",
+    subtitle: "Full Stack Developer",
+    description:
+      "Experienced in both frontend and backend development. Enjoys working on challenging projects and collaborating with teams.",
+    image: img3,
+  },
+];
